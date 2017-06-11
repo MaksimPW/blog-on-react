8c4bbc05d66342cad52e08494540b09a271fa40c
MaksimPW <mail@maksim.pw>
2017-06-11 09:58:31 +0700

    lesson-2

--- components/Image.js
+++ components/Image.js
@@ -1,11 +1,18 @@
 const Image = (props) => (
-  <img src={props.src} width={props.width} height={props.height} alt={props.alt}/>
+  <img {...props} />
 );
 
 
 Image.defaultProps = {
   src: 'http://maksim.pw/wp-content/uploads/2016/09/05OB6lDjCQvsXlwMc-C_T_qMMU3qjtEZ3Uz18-9f3kiewcVM-508x381.jpg',
   alt: 'default alt',
-  width: '200',
-  height: '200',
+  width: '200px',
+  height: '200px'
 };
+
+Image.propTypes = {
+  src: PropTypes.string,
+  alt: PropTypes.string,
+  width: PropTypes.string,
+  height: PropTypes.string
+}
