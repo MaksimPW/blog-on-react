efbe3773e43a8a6d2f4a222ee5b343a0d27b821f
MaksimPW <mail@maksim.pw>
2017-08-14 21:50:19 +0700

    lesson-5 | fix eslint

--- src/client/app/helpers/Api.js
+++ src/client/app/helpers/Api.js
@@ -1,10 +1,8 @@
-const Api = {
+const Api = ({
   hostname: 'http://localhost:3000',
   index: 'http://localhost:3000/posts',
-  addLike: (id) => {
-    return 'http://localhost:3000/posts/'.concat(id, '/add_like')
-  },
+  addLike: (id) => 'http://localhost:3000/posts/'.concat(id, '/add_like'),
   defaultImage: 'http:///localhost:3000/images/original/missing.png'
-}
+});
 
 export default Api;
