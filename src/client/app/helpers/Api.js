06a1f5b4890469f34f4fede5ed6cca035a2deb7f
MaksimPW <mail@maksim.pw>
2017-08-15 22:43:56 +0700

    lesson-5 | fixes

--- src/client/app/helpers/Api.js
+++ src/client/app/helpers/Api.js
@@ -1,8 +1,8 @@
 const Api = ({
   hostname: 'http://localhost:3000',
   index: 'http://localhost:3000/posts',
-  addLike: (id) => 'http://localhost:3000/posts/'.concat(id, '/add_like'),
-  defaultImage: 'http:///localhost:3000/images/original/missing.png'
+  addLike: (id) => `http://localhost:3000/posts/${id}/add_like`,
+  defaultImage: 'http://localhost:3000/images/original/missing.png'
 });
 
 export default Api;
