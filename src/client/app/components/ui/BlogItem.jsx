d11f337af078c28434b7d953664ef99769373809
MaksimPW <mail@maksim.pw>
2017-08-23 13:53:29 +0700

    lesson-6 | LikeContainer props.count

--- src/client/app/components/ui/BlogItem.jsx
+++ src/client/app/components/ui/BlogItem.jsx
@@ -10,7 +10,7 @@ import LikeContainer from './../../containers/LikeContainer';
 
 import { postsPath } from './../../helpers/routes/paths';
 
-const BlogItem = ({ id, image, details, title, likes, postType }) => (
+const BlogItem = ({ id, image, details, title, postType }) => (
   <div className="blog_item">
     <Link to={postsPath(id)}>
       <Image {...image} />
@@ -18,7 +18,7 @@ const BlogItem = ({ id, image, details, title, likes, postType }) => (
     <div className="container">
       <DetailsBox {...details} />
       <TextBox>{title}</TextBox>
-      <LikeContainer count={likes} postId={id} postType={postType} />
+      <LikeContainer postId={id} postType={postType} />
     </div>
   </div>
 );
@@ -28,7 +28,6 @@ BlogItem.propTypes = {
   image: PropTypes.object,
   details: PropTypes.object,
   title: PropTypes.string,
-  likes: PropTypes.number,
   postType: PropTypes.string
 };
 
--- src/client/app/containers/LikeContainer.jsx
+++ src/client/app/containers/LikeContainer.jsx
@@ -1,13 +1,26 @@
 import { connect } from 'react-redux';
 import Like from './../components/ui/Like';
 
+import _ from 'lodash';
+
 import { fetchLikePost } from './../actions/Post';
 import { fetchLikePosts } from './../actions/Posts';
 
-const stateToProps = (state) => ({
+const stateToProps = (state, props) => ({
   error: state.post.error,
-  posts: state.posts.entries
+  posts: state.posts.entries,
+  count: countLikesProps(state, props)
 });
+
+const countLikesProps = (state, props) => {
+  const entries = _.cloneDeep(state.posts.entries);
+  const index = _.findIndex(entries, post => post.id == props.postId);
+  if (entries[index] == null) {
+    return state.post.entry.likes;
+  } else {
+    return entries[index].likes;
+  }
+};
 
 const actionsToProps = (dispatch) => ({
   likeAdd(postId, postType, posts) {
