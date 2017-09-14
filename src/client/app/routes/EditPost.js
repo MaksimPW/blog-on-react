1451d19afd9d0802a8470d2bbf1cda130dfa4f24
MaksimPW <mail@maksim.pw>
2017-09-14 19:10:56 +0700

    lesson-9 | load Post for editPost

--- src/client/app/routes/EditPost.js
+++ src/client/app/routes/EditPost.js
@@ -1,11 +1,18 @@
+import { fetchPost } from './../actions/Post';
 import { editPostsPath } from './../helpers/routes/paths';
+
+import initialLoad from './../helpers/initialLoad';
 
 import EditPostView from './../components/ui/Edit';
 
 const EditPostRoute = {
   exact: true,
   path: editPostsPath(),
-  component: EditPostView
+  component: EditPostView,
+  prepareData: (store, query, params) => {
+    if (initialLoad()) return;
+    return store.dispatch(fetchPost(params.id));
+  }
 };
 
 export default EditPostRoute;
