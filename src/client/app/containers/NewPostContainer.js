53f094fd8fe31a2fb0a33a9b325a2884e8e2007e
MaksimPW <mail@maksim.pw>
2017-09-16 23:42:58 +0700

    lesson-9 | bad commit for validate server

--- src/client/app/containers/NewPostContainer.js
+++ src/client/app/containers/NewPostContainer.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import { reduxForm } from 'redux-form';
+import { reduxForm, SubmissionError } from 'redux-form';
 
 import { createPost } from './../actions/Post';
 import NewPostView from './../components/ui/New';
@@ -11,10 +11,20 @@ const stateToProps = () => ({
   }
 });
 
+const onSubmit = (values, dispatch) => {
+  dispatch(createPost(values)).catch((response) => {
+    if (response.body.errors.length != 0) {
+      throw new SubmissionError(response.body.errors);
+    } else {
+      // TODO: Redirect to root path
+    }
+  }).catch((error) => {
+    console.log(error);
+  });
+}
+
 export default connect(stateToProps)(
   reduxForm({
     form: 'newPost',
-    onSubmit: (values, dispatch) => (
-      dispatch(createPost(values))
-    )
+    onSubmit
   })(NewPostView));
