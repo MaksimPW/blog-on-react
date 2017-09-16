6698c71ef06c39528c7074f7cf314f25ce993d0c
MaksimPW <mail@maksim.pw>
2017-09-16 15:48:30 +0700

    lesson-9 | fix initialize redux-form after api success

--- src/client/app/components/ui/Edit.js
+++ src/client/app/components/ui/Edit.js
@@ -29,36 +29,55 @@ renderField.propTypes = {
   meta: PropTypes.object
 };
 
-const EditPostView = ({ handleSubmit, pristine, submitting, reset }) => (
-  <div>
-    <h1>Edit Post</h1>
-    <form onSubmit={handleSubmit} className='ui form'>
-      <Field 
-        label='Title'
-        component={renderField}
-        type='text'
-        name='title'
-      />
-      <Field
-        label='Author'
-        component={renderField}
-        type='text'
-        name='author'
-      />
-      {
-        (!pristine && !submitting) &&
-        <button className='ui button' onClick={reset}>Clear</button>
-      }
-      <input type='submit' className='ui submit' value='Update' />
-    </form>
-  </div>
-);
+
+class EditPostView extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { post, isFetching } = this.props;
+    return (
+      !isFetching && post && this.renderEdit()
+    );
+  }
+
+  renderEdit() {
+    const { handleSubmit, pristine, submitting, reset } = this.props;
+    return (
+      <div>
+        <h1>Edit Post</h1>
+        <form onSubmit={handleSubmit} className='ui form'>
+          <Field
+            label='Title'
+            component={renderField}
+            type='text'
+            name='title'
+          />
+          <Field
+            label='Author'
+            component={renderField}
+            type='text'
+            name='author'
+          />
+          {
+            (!pristine && !submitting) &&
+            <button className='ui button' onClick={reset}>Clear</button>
+          }
+          <input type='submit' className='ui submit' value='Update' />
+        </form>
+      </div>
+    );
+  }
+}
 
 EditPostView.propTypes = {
   handleSubmit: PropTypes.func,
   pristine: PropTypes.bool,
   submitting: PropTypes.bool,
-  reset: PropTypes.func
+  reset: PropTypes.func,
+  post: PropTypes.object,
+  isFetching: PropTypes.bool
 };
 
 export default EditPostView;
