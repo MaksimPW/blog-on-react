2afb39b5217874859522c7fad8d70b905040b08d
MaksimPW <mail@maksim.pw>
2017-09-16 19:15:22 +0700

    lesson-9 | add Helmet for newPost

--- src/client/app/components/ui/New.js
+++ src/client/app/components/ui/New.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import Helmet from 'react-helmet';
 import PropTypes from 'prop-types';
 
 import FormPost from './../elements/FormPost';
 
 const NewPostView = ({ handleSubmit, pristine, submitting, reset }) => (
-  <FormPost
-    label='Create'
-    handleSubmit={handleSubmit}
-    pristine={pristine}
-    submitting={submitting}
-    reset={reset}
-  />
+  <div>
+    <FormPost
+      label='Create'
+      handleSubmit={handleSubmit}
+      pristine={pristine}
+      submitting={submitting}
+      reset={reset}
+    />
+    <Helmet title="Создать новый пост" >
+      <meta name="keywords" content="новый пост автор" />
+      <meta name="description" content="Создание нового поста" />
+    </Helmet>
+  </div>
 );
 
 NewPostView.propTypes = {
