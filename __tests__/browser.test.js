7434a40695340bcfa11da6aab19fa360164951ad
MaksimPW <mail@maksim.pw>
2017-10-03 06:51:43 +0700

    lesson-10 | up tests

--- __tests__/browser.test.js
+++ __tests__/browser.test.js
@@ -1,21 +1,33 @@
 import nightmare from 'nightmare'
 
 describe('When visiting the homepage', () => {
-
-  it('it welcomes the user', async () => {
+  it('displays content head', async () => {
     let page = nightmare().goto('http://localhost:3030')
-
-    let text = await page.evaluate(() => document.body.textContent)
-                         .end()
+    let text = await page.evaluate(() => document.body.textContent).end();
 
     expect(text).toContain('Blog')
   })
 
   it('displays page title', async () => {
     let page = nightmare().goto('http://localhost:3030');
-
     let title = await page.evaluate(() => (document.title)).end();
 
     expect(title).toContain('Список постов')
+  })
+})
+
+describe('When visiting the contacts', () => {
+  it('displays content head', async () => {
+    let page = nightmare().goto('http://localhost:3030').click('a[href$="/contacts"]');
+    let text = await page.evaluate(() => document.body.textContent).end();
+
+    expect(text).toContain('Contacts')
+  })
+
+  it('displays page title', async () => {
+    let page = nightmare().goto('http://localhost:3030').click('a[href$="/contacts"]');
+    let title = await page.evaluate(() => (document.title)).end();
+
+    expect(title).toContain('Обратная связь')
   })
 })
